180a3b4cf4d38e4dcb793c92c5ba7355242be78d
Karn Kaul <karnkaul@gmail.com>
2023-04-04 21:10:20 -0700

    :bug: Don't store future in base class
    - If base stores the future, derived class instances get destroyed before it, risking dangling access in `execute()`, before the future in the base object blocks in its destructor.

--- levk/include/levk/asset/asset_list_loader.hpp
+++ levk/include/levk/asset/asset_list_loader.hpp
@@ -5,15 +5,21 @@
 
 namespace levk {
 class AssetProviders;
+class ThreadPool;
 
 class AssetListLoader : public AsyncTask<void> {
   public:
-	AssetListLoader(NotNull<AssetProviders const*> asset_providers, AssetList list) : m_asset_providers(asset_providers), m_list(std::move(list)) {}
+	AssetListLoader(NotNull<AssetProviders const*> asset_providers, AssetList list, Ptr<ThreadPool> thread_pool = {})
+		: m_list(std::move(list)), m_asset_providers(asset_providers), m_thread_pool(thread_pool) {}
 
   protected:
+	std::future<void> const& get_future() const override { return m_future; }
 	void execute() override;
 
-	NotNull<AssetProviders const*> m_asset_providers;
 	AssetList m_list{};
+	NotNull<AssetProviders const*> m_asset_providers;
+	Ptr<ThreadPool> m_thread_pool{};
+
+	std::future<void> m_future{};
 };
 } // namespace levk
